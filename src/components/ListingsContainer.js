b7ce7e33b0fa64369f1f6153b8ddb9f726c78af7
Kevin Diesenberg <kdiesenb@gmail.com>
2022-11-29 11:43:51 -0500

    sorting by location works

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -3,6 +3,7 @@ import ListingCard from "./ListingCard";
 
 function ListingsContainer({ search }) {
   const [listings, setListings] = useState([]);
+  const [sortBy, setSortBy] = useState("id");
 
   useEffect(() => {
     fetch("http://localhost:6001/listings")
@@ -19,7 +20,15 @@ function ListingsContainer({ search }) {
     return listing.description.toLowerCase().includes(search.toLowerCase());
   });
 
-  const listingCards = filteredListingsArr.map((listing) => {
+  const sortedListings = filteredListingsArr.sort((listingA, listingB) => {
+    if (sortBy === "id") {
+      return listingA.id - listingB.id;
+    } else {
+      return listingA.location.localeCompare(listingB.location);
+    }
+  })
+
+  const listingCards = sortedListings.map((listing) => {
     return (
       <ListingCard
         key={listing.id}
@@ -28,9 +37,23 @@ function ListingsContainer({ search }) {
       />
     );
   });
+  /*
+  As a user:
+I can sort the listings alphabetically by location.
+I can create a new listing by submitting a form, and persist the changes to
+the backend.
+create UI since none exist.  Logical place would be in the listings div
+  ui would a button >sort locations< on click it sorts
+  pulls listings from the filteredListingsArr because it contains the visible listings after possible deletions
+  create a new state for the sorted listings
+  */
 
   return (
     <main>
+      <button onClick={() => setSortBy("id")}>Default Sort</button>
+      <button onClick={() => setSortBy("location")}>
+        Sort Locations
+      </button>
       <ul className="cards">{listingCards}</ul>
     </main>
   );
